950655dc0d7048846a465ca11d94359226308283
Johangel Leon <johangel.leon@j2solutionsit.com>
2019-01-26 20:42:47 -0400

    many changes

--- js/gameConfig.js
+++ js/gameConfig.js
@@ -1,58 +1,106 @@
 const frameRate = 25
 const frameRateSecMult = 40
 var timeSpend = 0;
+var score = 0;
 
 var areaJuego = {
-    canvas : document.createElement("canvas"),
-    start : function(){
+    canvas: document.createElement("canvas"),
+    start: function () {
         this.canvas.classList.add('areaGame')
         this.canvas.width = 1000;
         this.canvas.height = 500;
         this.framenro = 1;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(x=>{
+        this.interval = setInterval(x => {
             updateGameArea()
         }, frameRate);
 
     },
-    clear : function() {
-     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    reset: function () {
+        console.log('entro')
+        clearInterval(this.interval)
+        timeSpend = 0;
+        score = 0;
+        updateGameTime(timeSpend * -1)
+        this.clear()
     }
- }
+}
 
- function component(width, height, cabeza, x, y, type){
+function component(width, height, cabeza, x, y, type) {
     this.type = type;
     this.height = height;
     this.width = width;
     this.nombre = cabeza;
     this.cabeza = new Image();
-    this.cabeza.src = 'caritas/'+cabeza+'.png';
+    this.cabeza.src = 'caritas/' + cabeza + '.png';
     this.x = x;
     this.y = y;
-    this.text ="jej";
+    this.text = "jej";
     this.primeraCarga = 0;
-    this.update = function(x,y){
-        this.x=x;
-        this.y=y;
+    this.update = function (x, y) {
+        this.x = x;
+        this.y = y;
         ctx = areaJuego.context;
-        if(this.type == "personaje"){
- 
-            if(this.primeraCarga == 0){
-                this.cabeza.onload = function(){
-                ctx.drawImage(Personaje.cabeza, x, y, 100, 100);
+        if (this.type == "personaje") {
+
+            if (this.primeraCarga == 0) {
+                this.cabeza.onload = function () {
+                    ctx.drawImage(Personaje.cabeza, x, y, 100, 100);
                 }
                 this.primeraCarga = 1;
             }
- 
-             ctx.drawImage(this.cabeza, x, y, 100, 100);
-             var cuerpox = x+10;
-             if(this.nombre == "rosita"){cuerpox = x;}
-             var cuerpoy = y +95;
-             ctx.drawImage(imagenDeCuerpito,cuerpox,cuerpoy,70,70);
+
+            ctx.drawImage(this.cabeza, x, y, 100, 100);
+            var cuerpox = x + 10;
+            if (this.nombre == "rosita") {
+                cuerpox = x;
+            }
+            var cuerpoy = y + 95;
+            ctx.drawImage(imagenDeCuerpito, cuerpox, cuerpoy, 70, 70);
         }
-        if(this.type == "comida"){
+        if (this.type == "comida") {
             ctx.drawImage(this.cabeza, x, y, 50, 50);
         }
     }
- }
+    this.checkCollision = function (objeto) {
+        // console.log('esto es una manzana: ', objeto)
+        if ((-100 < (Personaje.x - objeto.x)) &&
+            ( 100 > (Personaje.x - objeto.x)) &&
+            ( 100 > (Personaje.y - objeto.y)) &&
+            (-100 < (Personaje.y - objeto.y))){
+            createNewApple()
+            updateScore(1)
+        }
+    }
+}
+
+function updateGameTime(value) {
+    timeSpend = timeSpend + value;
+    document.getElementById('timeFrame').innerHTML = timeSpend + ' segs';
+}
+
+function updateScore(value){
+    score = score + value
+    document.getElementById('scoreBoard').innerHTML = score;
+
+}
+
+function drawInfo(objeto){
+    if(objeto.type == 'comida'){
+        document.getElementById('manzanaInfo').innerHTML = JSON.stringify(objeto)
+    }
+    if(objeto.type == 'personaje'){
+        document.getElementById('personajeInfo').innerHTML = JSON.stringify(objeto)
+    }
+}
+
+function createNewApple(){
+        x = areaJuego.canvas.width * Math.random();
+       y = areaJuego.canvas.height * Math.random();
+       Comida[0] = new component(20, 20, 'manzana', x, y, 'comida');
+       drawInfo(Comida[0])
+}
--- js/main.js
+++ js/main.js
@@ -7,35 +7,46 @@ imagenDeCuerpito.src = "caritas/cuerpito.png";
   document.addEventListener("keydown",function(event){
                if(event.keyCode == 40){
                    Personaje.update(Personaje.x, Personaje.y+15);
+                   document.getElementById('downKey').classList.add('pressedButon')
                }else if(event.keyCode == 39){
                      Personaje.update(Personaje.x+15, Personaje.y);
+                     document.getElementById('rightKey').classList.add('pressedButon')
                }else if(event.keyCode == 38){
                      Personaje.update(Personaje.x, Personaje.y-15);
+                     document.getElementById('upKey').classList.add('pressedButon')
                }else if(event.keyCode == 37){
                      Personaje.update(Personaje.x-15, Personaje.y);
+                     document.getElementById('leftKey').classList.add('pressedButon')
                }
+               drawInfo(Personaje)
         });
 
-function comenzarJuego(){
+    document.addEventListener('keyup',x=>{
+        arrayElement = document.getElementsByClassName('pressedButon')
+        for (let item of arrayElement) {
+            item.classList.remove('pressedButon')
+        } 
+        
+        
+    })
 
-
+function restartGame(){
+    areaJuego.reset()
 }
 
 function updateGameArea(){
    areaJuego.clear();
    Personaje.update(Personaje.x, Personaje.y);
    areaJuego.framenro = areaJuego.framenro + 1;
-   if(areaJuego.framenro === 2 || intervalo(frameRateSecMult * 10)){
-       x = areaJuego.canvas.width * Math.random();
-       y = areaJuego.canvas.height * Math.random();
-       Comida[0] = new component(20, 20, 'manzana', x, y, 'comida');
-       console.log(Comida)
+   if(areaJuego.framenro === 2 || intervalo(frameRateSecMult * 100)){
+       createNewApple()
     }
     if(areaJuego.framenro % frameRateSecMult == 0){
-        timeSpend = timeSpend + 1;
-        document.getElementById('timeFrame').innerHTML = timeSpend;
+        updateGameTime(1)
     }
     Comida[0].update(Comida[0].x, Comida[0].y)
+    Personaje.checkCollision(Comida[0])
+    
 }
 
 function checkKey(e) {
@@ -44,9 +55,12 @@ console.log(event.keyCode)
 }
 
 function seleccionarPersonaje(e){
-   console.log(e);
+    if(timeSpend > 0){
+        areaJuego.reset()
+    }
    carita = String(document.getElementById("listaPersonajes").value);
    Personaje = new component("200px","400px",carita,100, 100, "personaje");
+   drawInfo(Personaje)
    areaJuego.start();
    Personaje.update(Personaje.x, Personaje.y);
 }
